8c83de277f88ec521aa0131d5e61567403bd2505
Karla Ivankovic <39747361+ivkarla@users.noreply.github.com>
2023-03-01 16:09:01 +0100

    Update game.py

--- src/game.py
+++ src/game.py
@@ -31,18 +31,18 @@ def analyze(set, nodes, kratio=.1, random_state=31, **mopts):
     cvs = cross_val_score(C, X, Y, cv=cv)
     return cvs
 
-def maximin_of(results):
-    """Computes the maximin of an array of numbers.
+def evaluation_function(results):
+    """Heuristic evaluation function.
 
     Args:
         results (ndarray): Cross validation score array.
 
     Returns:
-        float: Maximin of results.
+        float: evaluation of results.
     """
     return max(results)*(min(results)/np.average(results)) 
 
-def enlist(nodes, labels, results, symbol='<->', fx=maximin_of):
+def evaluate_nodes(nodes, labels, results, symbol='<->', f=evaluation_function):
     """Service function. 
     Creates sets with node indices, labels, cross validation scores and a reference function of the scores.
 
@@ -51,19 +51,19 @@ def enlist(nodes, labels, results, symbol='<->', fx=maximin_of):
         labels ([type]): [description]
         results (ndarray): Cross validation score array.
         symbol (str): Symbol used to connect node labels within a network. Defaults to '<->'.
-        fx (function): Function of cross validation scores used to rank the networks based on their connectivity change. Defaults to maximin_of.
+        f (function): Function of cross validation scores used to rank the networks based on their connectivity change. Defaults to evaluation_function.
 
     Returns:
         set: A set of all the arguments.
     """
     tag = symbol.join([labels[n] for n in nodes])
-    return (nodes, tag, results, fx(results))
+    return (nodes, tag, results, f(results))
 
 def check_until(net, set=1, fall=0, at=-1):
-    """This function takes a list of analyzed node combinations sorted by their maximin score.
+    """This function takes a list of analyzed node combinations sorted by their evaluation score.
     The function defines an index of the list until which the node combinations are considered epileptogenic.
 
-    Node combination are iterated and the average maximin score for each combination is computed. 
+    Node combination are iterated and the average evaluation score for each combination is computed. 
     The score is used as a threshold, above which epileptogenic nodes are selected.
     When the score decreases the iteration stops.
 
