24af09c413a44a42c53b1aa348e0b22de43aa09b
Karla Ivankovic <39747361+ivkarla@users.noreply.github.com>
2023-03-09 09:44:48 +0100

    Update game.py

--- src/game.py
+++ src/game.py
@@ -59,7 +59,7 @@ def evaluate_nodes(nodes, labels, results, symbol='<->', f=evaluation_function):
     tag = symbol.join([labels[n] for n in nodes])
     return (nodes, tag, results, f(results))
 
-def check_until(net, set=1, fall=0, at=-1):
+def check_until(net, set=1, fall=0, at=-1, limit=None):
     """This function takes a list of analyzed node combinations sorted by their evaluation score.
     The function defines an index of the list until which the node combinations are considered epileptogenic.
 
@@ -70,14 +70,17 @@ def check_until(net, set=1, fall=0, at=-1):
     Args:
         net (list): A list of sets returned by evaluate_nodes().
         set (int): Defaults to 1.
-        fall (int): Defaults to 0.
-        at (int): Defaults to -1.
+        fall (int): Define score threshold (should be the highest score). Defaults to 0.
+        at (int): Denotes the index of the set, containing the results for a node group. Defaults to -1, as this is the current saving format.
+        limit (int): Define limit until which the nodes are checked. This could be useful for checking node pairs (if many node pairs have the highest score, the WOI could be uninformative).
 
     Returns:
         int: Index of net list.
     """
     print("Searching for the index of the last best network...")
-    while set < len(net):
+    l = len(net)
+    if limit: l = limit
+    while set < l:
         print("Net =", net[:set])
         print("Score =", [n[at] for n in net[:set]][-1])
         score = [n[at] for n in net[:set]][-1]
@@ -85,5 +88,5 @@ def check_until(net, set=1, fall=0, at=-1):
             fall=score
             set+=1
         else: break
-    print(f"Last best network at index {set-1}")
+    print(f"Last best network at index {set-2}")
     return set-1
