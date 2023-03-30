412a23fbf9bd14ec8b06ecb85cd010e7a7ed8f45
Karla Ivankovic <39747361+ivkarla@users.noreply.github.com>
2023-03-30 18:17:17 +0200

    Working version

--- shell/job_connectivity.sh
+++ shell/job_connectivity.sh
@@ -6,14 +6,10 @@
 #SBATCH --chdir=/homedtic/rzucca/epigame
 #SBATCH --time=2:00
 #SBATCH --array=1-21:1
-#SBATCH --mem=4G
 #SBATCH --sockets-per-node=1
 #SBATCH --cores-per-socket=2
 #SBATCH --threads-per-core=2
 #SBATCH -o %N.%J.out #STDOUT
 #SBATCH -e %N.%J.err #STDERR
 
-INPUT_SEIZURE = $(ls /homedtic/rzucca/data/seizure/${SLURM_ARRAY_TASK_ID}-seizure.EDF)
-INPUT_BASELINE = $(ls /homedtic/rzucca/data/baseline/${SLURM_ARRAY_TASK_ID}-baseline.EDF)
-
-python3 /homedtic/rzucca/epigame/code/connectivity_matrices_2.py $INPUT_SEIZURE $INPUT_BASELINE 2
+python3 /homedtic/rzucca/epigame/code/connectivity_matrices.py /homedtic/rzucca/epigame/data/${SLURM_ARRAY_TASK_ID}-preseizure5.prep
