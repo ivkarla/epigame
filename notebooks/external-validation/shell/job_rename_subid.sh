6710819d84ba099b2722de83fbe499246a2b7bb2
Karla Ivankovic <39747361+ivkarla@users.noreply.github.com>
2024-02-12 13:20:19 +0100

    Update job_rename_subid.sh

--- notebooks/external-validation/shell/job_rename_subid.sh
+++ notebooks/external-validation/shell/job_rename_subid.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH -J FNAME
-#SBATCH -p short
-#SBATCH -N 1
-#SBATCH --chdir=/homes/users/kivankovic/ext-val
-#SBATCH --time=2:00
-#SBATCH --sockets-per-node=1
-#SBATCH --cores-per-socket=2
-#SBATCH --threads-per-core=2
-#SBATCH -o %N.%J.out #STDOUT
-#SBATCH -e %N.%J.err #STDERR
+#SBATCH -J tab
+#SBATCH -p normal
+#SBATCH -N 2
+#SBATCH -n 4
+#SBATCH --mem=16GB
+#SBATCH -t 0-2:00
+
+#SBATCH -o %J_%A_%a.out #STDOUT
+#SBATCH -e %J_%A_%a.err #STDERR
 
 python3 /homes/users/kivankovic/ext-val/scripts/rename_sub_id.py /homes/users/kivankovic/ext-val/
